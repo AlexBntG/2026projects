f3f6ebe8ea75f441ac996518d767e0fa1b637edc
AlexBntG <bontealex01@gmail.com>
2026-09-10 11:31:42 +0300

    Add Git practice message

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, GitHub!" << std::endl;
     std::cout << "I am learning C++ and Git." << std::endl;
+    std::cout << "I am practicing Git." << std::endl;
     return 0;
 }
