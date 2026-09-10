72b896ff97fb865cb160e02184a588052ec16af7
AlexBntG <bontealex01@gmail.com>
2026-09-10 11:35:11 +0300

    Revert "Add Git practice message"
    This reverts commit f3f6ebe8ea75f441ac996518d767e0fa1b637edc.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,6 +3,5 @@
 int main() {
     std::cout << "Hello, GitHub!" << std::endl;
     std::cout << "I am learning C++ and Git." << std::endl;
-    std::cout << "I am practicing Git." << std::endl;
     return 0;
 }
